b6d3eb4d2720267b04cb00d86df25e7ee073421a
Matheus luan <matheusluan04@gmail.com>
2026-05-15 20:43:13 -0300

    Inicio de atividade

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("digite o valor total da compra:");
+decimal valor = Console.Read();
+
+if (valor >= 0)
+{
+    Console.Write("Digite um valor valido");
+}
+else
+{
+    Console.Write("O VALOR ESTÁ CORRETO" );
+}
